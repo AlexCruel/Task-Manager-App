e047050edb4a55af8919c2428b586f31c4e95f58
Alex <sasha.karpenkov@outlook.com>
2020-02-01 14:32:55 +0300

    async/await in the index.js

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e2f2c0809bb370eb067e9a0').then((user) => {
-    console.log(user)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5e2f2c0809bb370eb067e9a0').then((user) => {
+//     console.log(user)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const findByIdAndDelete = async(id, completed) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments(completed)
+    return count
+}
+
+findByIdAndDelete('5e2f2c17c968951a4828c881', false).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
