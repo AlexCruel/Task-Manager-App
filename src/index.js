a15c21560502da17111c1056d362cf4c7ad2f9d9
Alex <sasha.karpenkov@outlook.com>
2020-02-05 19:55:56 +0300

    added cascade deleting tasks

--- src/index.js
+++ src/index.js
@@ -25,3 +25,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async() => {
+    // const task = await Task.findById('5e3ada17d670d8058883c809')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5e3ad826dade181eb4bb85b8')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
